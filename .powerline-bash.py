66046b560f1b22a494b3aa34756654777999d9ae
Joshua Appelman <joshua@jbna.nl>
2014-05-23 16:40:40 +0200

    Decrements maximum path depth on powerline to three before ellipsis.

--- .powerline-bash.py
+++ .powerline-bash.py
@@ -335,7 +335,7 @@ if __name__ == '__main__':
     add_virtual_env_segment(p, cwd)
     #p.append(Segment(p, ' \\u ', 250, 240))
     #p.append(Segment(p, ' \\h ', 250, 238))
-    add_cwd_segment(p, cwd, 4, args.cwd_only)
+    add_cwd_segment(p, cwd, 3, args.cwd_only)
     add_repo_segment(p, cwd)
     add_root_indicator(p, args.prev_error)
     sys.stdout.write(p.draw())
